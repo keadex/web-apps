f5410c093240706ff6cb8bd02ecb00ae7329d86d
Keadex <12099977+keadex@users.noreply.github.com>
2022-05-14 00:55:20 +0200

    feat(keadex-mina): 🎸 added services architecture

--- apps/keadex-mina/src-tauri/src/main.rs
+++ apps/keadex-mina/src-tauri/src/main.rs
@@ -8,14 +8,18 @@ pub mod repository;
 use crate::controller::diagram_controller::DiagramController;
 use env_logger::{Builder, Env};
 use log::{LevelFilter};
+use state::Storage;
+use std::sync::RwLock;
+
+static DIAGRAM_CONTROLLER: Storage<RwLock<DiagramController>> = Storage::new();
 
 
 
 #[tauri::command]
 fn my_custom_command(invoke_message: String) {
-  let controller = DiagramController::new();
-  println!("{}", controller.give_me_name(&invoke_message[..]));
-  println!("{}", controller.diagram_repository.give_me_name(&invoke_message[..]));
+  //let controller = DiagramController::new();
+  println!("{}", DIAGRAM_CONTROLLER.get().read().unwrap().give_me_name(&invoke_message[..]));
+  println!("{}", DIAGRAM_CONTROLLER.get().read().unwrap().diagram_repository.give_me_name(&invoke_message[..]));
   // println!("I was invoked from JS, with this message: {}", invoke_message);
 }
 
@@ -27,8 +31,13 @@ fn init_logger() {
     .init();
 }
 
+fn init_services() {
+  DIAGRAM_CONTROLLER.set(RwLock::new(DiagramController::new()));
+}
+
 fn main() {
   init_logger();
+  init_services();
   log::info!("a log from `MyLogger`");
   println!("hello there!");
   tauri::Builder::default()
